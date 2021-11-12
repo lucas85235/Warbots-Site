01adbab0666708d018304ef1e88fcbb524daf726
Lucas Lima <lucas85235@gmail.com>
2021-11-12 18:06:25 -0400

    Fix target build error

--- components/BottonButton.jsx
+++ components/BottonButton.jsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 export default function BottonButton(props) {
     return (
-        <a href={props.link} target="_blank">
+        <a href={props.link} target="_blank" rel="noopener noreferrer">
             <button className={styles.button}>
                 <Image src={props.image} alt="" width="75px" height="75px" />
             </button>
