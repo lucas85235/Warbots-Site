a91674696ab1dbe21fba71610636c4eacb9c6e85
Lucas Lima <lucas85235@gmail.com>
2022-08-23 09:57:21 -0400

    Improve Navbar

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -34,10 +34,10 @@ const secondaryLinks = [
     },
 ];
 
-function CreateButtons() {
+function CreateButtons(listLinks) {
     return (
         <div>
-            {secondaryLinks.map(data =>
+            {listLinks.map(data =>
                 <Button href={data.href} primary={data.primary} target={data.target}>
                     {data.label}
                 </Button>
@@ -48,8 +48,30 @@ function CreateButtons() {
 
 export default function RenderMenu() {
     return (
-        <div className={styles.image}>
-            {CreateButtons()}
-        </div>
+        <nav className={styles.nav}>
+            <div className={styles.container}>
+                <div className={styles.items}>
+
+                    <div className={styles.divLeft}>
+                        <div className={styles.divLeftLogo}>
+                            <img src="/warbots-logo.png" alt="" width="97px" height="53px"/>
+                        </div>
+                        <div className={styles.divButtons}>
+                            <div className={styles.buttonsList}>
+                                {CreateButtons(links)}
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className={styles.divButtons}>
+                        <div className={styles.buttonsList}>
+                            {CreateButtons(secondaryLinks)}
+                        </div>
+                    </div>
+
+                </div>
+            </div>            
+        </nav>
+
     )
 }
