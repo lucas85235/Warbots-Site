19337c0c9996282a5fbf398e322da8b3ff7f54df
Lucas Lima <lucas85235@gmail.com>
2022-08-23 11:01:53 -0400

    Make Navbar mobile responsive

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import styles from '../styles/NavBar.module.css'
 import Button from './Button';
 
@@ -19,17 +20,17 @@ const secondaryLinks = [
     {
         label: "Play Demo",
         href: "https://darksardinha.itch.io/warbots",
-        target: "_blanck",
+        target: "_blank",
     },
     {
         label: "Whitepaper",
         href: "https://lucas85235.gitbook.io/warbots-whitepaper/",
-        target: "_blanck",
+        target: "_blank",
     },
     {
         label: "Whitelist",
         href: "https://forms.gle/XHGCCr3h6BvmfnHb6",
-        target: "_blanck",
+        target: "_blank",
         primary: true
     },
 ];
@@ -46,7 +47,81 @@ function CreateButtons(listLinks) {
     )
 }
 
+function MenuButton({ toggleMenu, showMenu }) {
+    return (
+        <button
+            type="button"
+            aria-controls="mobile-menu"
+            aria-expanded={showMenu}
+            onClick={toggleMenu}
+            className={styles.mobileButton}
+        >
+            <span className={styles.srOnly}>Open menu</span>
+            {showMenu ? (
+                <svg
+                    className={styles.svgLength}
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                    width={24}
+                    height={24}
+                >
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+                </svg>
+            ) : (
+                <svg
+                    className={styles.svgLength}
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                    width={24}
+                    height={24}
+                >
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
+                </svg>
+            )}
+
+        </button>
+    )
+}
+
+function MobileMenu() {
+    return (
+        <div className={styles.mobile}>
+            <div className={styles.mobileList1}>
+                {links.map((link) => (
+                    <a href={link.href} target={link.target} className={styles.mobileButton} key={link.label}>
+                        {link.label}
+                    </a>
+                ))}
+            </div>
+
+            <div className={styles.mobileList2}>
+                <div className={styles.mobileList2Div}>
+                    {secondaryLinks.map((link) => (
+                        <a
+                            key={`mobile-${link.label}`}
+                            href={link.href}
+                            target={link.target}
+                            className={styles.mobileButton}
+                        >
+                            {link.label}
+                        </a>
+                    ))}
+                </div>
+            </div>
+        </div>
+    )
+}
+
 export default function RenderMenu() {
+    const [showMenu, setShowMenu] = useState(false);
+    const toggleMenu = () => setShowMenu(!showMenu);
+
     return (
         <nav className={styles.nav}>
             <div className={styles.container}>
@@ -54,7 +129,7 @@ export default function RenderMenu() {
 
                     <div className={styles.divLeft}>
                         <div className={styles.divLeftLogo}>
-                            <img src="/warbots-logo.png" alt="" width="97px" height="53px"/>
+                            <img src="/warbots-logo.png" alt="" width="97px" height="53px" />
                         </div>
                         <div className={styles.divButtons}>
                             <div className={styles.buttonsList}>
@@ -62,15 +137,20 @@ export default function RenderMenu() {
                             </div>
                         </div>
                     </div>
-
+                    
                     <div className={styles.divButtons}>
                         <div className={styles.buttonsList}>
                             {CreateButtons(secondaryLinks)}
                         </div>
                     </div>
 
+                    <div className={styles.divToggle}>
+                        <MenuButton showMenu={showMenu} toggleMenu={toggleMenu} />
+                    </div>
+
                 </div>
-            </div>            
+            </div>
+            {showMenu ? <MobileMenu /> : null}
         </nav>
 
     )
