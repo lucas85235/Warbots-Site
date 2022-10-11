a0953cab150681e3d76144a9cbb8b57943d22e87
Lucas Lima <lucas85235@gmail.com>
2022-10-11 10:30:28 -0400

    Fix build Error

--- components/Gameplay.jsx
+++ components/Gameplay.jsx
@@ -40,7 +40,7 @@ function Gameplay() {
             <div style={{ marginTop: "3rem", marginBottom: "3.5rem" }}>
                 <h1 style={{ textAlign: "center" }}>Gameplay</h1>
                 <p style={textStyle}>
-                    Each robot is made up of 5 parts: the head, the body, the right arm, the left arm, and the legs. These are the parts that will define your Warbot's stats and attacks. These parts are NFTs, which can be purchased, traded and sold by the player community.
+                    Each robot is made up of 5 parts: the head, the body, the right arm, the left arm, and the legs. These are the parts that will define your Warbot&apos;s stats and attacks. These parts are NFTs, which can be purchased, traded and sold by the player community.
                 </p>
                 <p style={textStyle}>
                     Warbots adopts a game model called “play-to-earn”, that is, play to win. The player will be able to face both PvE and PvP modes to be rewarded with In-game Tokens! These Tokens can be used both within the game, for the acquisition of new parts and upgrades, as well as being withdrawn by the user.
