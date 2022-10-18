8322de6f21030b7f5a4be810ed2faff9cb705e05
Lucas Lima <lucas85235@gmail.com>
2022-10-18 18:07:31 -0400

    Gameplay screen shots

--- components/Gameplay.jsx
+++ components/Gameplay.jsx
@@ -1,51 +1,71 @@
+import styles from '../styles/Gameplay.module.css'
+import { useEffect, useState } from 'react'
 import Image from 'next/image'
 
 function Gameplay() {
 
-    const textStyle = {
-        display: "flex",
-        textAlign: "center",
-        color: "#444",
-        fontSize: "1.1rem",
-        fontWeight: "500",
-        paddingLeft: "25px",
-        paddingRight: "25px",
-        maxWidth: "60rem",
-        marginTop: "2rem",
-        marginBottom: "2rem"
-    }
+    const [selected, setSelected] = useState(0)
 
-    function ImageDiv() {
+    const imageList = [
+        "/screenshots/menu.png",
+        "/screenshots/game.png",
+        "/screenshots/office.png",
+    ]
+
+    const Buttons = () => {
         return (
-            <div style={{
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'center',
-                marginBottom: '2rem',
-                marginTop: '2em',
-            }}>
-                <Image src="/screenshots/game.png" width={480} height={270} alt="" style={{ borderRadius: '1rem' }} />
+            <div>
+                {imageList.map((value, key) =>
+                    <button
+                        key={key}
+                        className={`${styles.indicatorsItem} ${key == selected ? styles.active : ""}`}
+                        onClick={() => setSelected(key)}>
+                    </button>
+                )}
             </div>
         )
     }
 
+    const Next = () => {
+        var i = (selected - 1) < 0 ? imageList.length - 1 : selected - 1;
+        setSelected(i)
+    }
+
+    const Prev = () => {
+        var i = (selected + 1) >= imageList.length ? 0 : selected + 1;
+        setSelected(i)
+    }
+
     return (
-        <div style={{
-            minHeight: "40vh",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            flexDirection: "column",
-        }}>
-            <div style={{ marginTop: "3rem", marginBottom: "3.5rem" }}>
-                <h1 style={{ textAlign: "center" }}>Gameplay</h1>
-                <p style={textStyle}>
+        <div className={styles.container}>
+            
+            <div className={styles.div}>
+                <h1 className={styles.h1}>Gameplay</h1>
+                <p className={styles.textStyle}>
                     Each robot is made up of 5 parts: the head, the body, the right arm, the left arm, and the legs. These are the parts that will define your Warbot&apos;s stats and attacks. These parts are NFTs, which can be purchased, traded and sold by the player community.
                 </p>
-                <p style={textStyle}>
+                <p className={styles.textStyle}>
                     Warbots adopts a game model called “play-to-earn”, that is, play to win. The player will be able to face both PvE and PvP modes to be rewarded with In-game Tokens! These Tokens can be used both within the game, for the acquisition of new parts and upgrades, as well as being withdrawn by the user.
                 </p>
             </div>
+
+            <div className={styles.carousel}>
+                <div className={styles.carouselinner}>
+                    <Image className={styles.carouselItem} src={imageList[selected]} layout="fill" objectFit='contain'/>
+                </div>
+
+                <button className={styles.controlNext} onClick={Next}>
+                    <span className={styles.nextIcon}></span>
+                </button>
+                <button className={styles.controlPrev} onClick={Prev}>
+                    <span className={styles.prevIcon}></span>
+                </button>
+
+                <div className={styles.carouselIndicators}>
+                    {Buttons()}
+                </div>
+            </div>
+
         </div>
     )
 }
