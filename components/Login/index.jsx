2ad296397e3429dadc76238b15489f2016fdc71b
Lucas Lima <lucas85235@gmail.com>
2022-10-19 10:43:01 -0400

    Add lootboxs in popup

--- components/Login/index.jsx
+++ components/Login/index.jsx
@@ -1,15 +1,40 @@
 import styles from "../../styles/Login.module.css"
 import { useBlockchain } from '../../blockchain'
+import Image from "next/image"
 import PartListView from "../PartListView";
 import Button from "../Button"
 import Mint from "./Mint"
 
 function Login() {
 
-    const { isLoged, myTokens, doLogin } = useBlockchain()
+    const { isLoged, myTokens, doLogin, doMint } = useBlockchain()
+
+    const eventsHandle = {
+        onRegistered: (hash) => { handleRegistered(hash) },
+        onReceipt: (receipt) => { handleReceipt(receipt) },
+        onConfirmation: (confirmationNumber, receipt) => { },
+        onError: (err) => { handleError(err) },
+    }
+
+    const handleRegistered = async (hash) => {
+        alert("Transaction Submitted! \nHash: " + hash)
+    }
+    const handleReceipt = async (receipt) => {
+        alert("Your nft is minted with success!")
+    }
+    const handleError = async (err) => {
+        alert(err.message)
+    }
 
     const handleLogin = async () => {
         await doLogin()
+    }
+
+    const handleMint = async () => {
+        if (isLoged) {
+            await doMint("1", eventsHandle)
+        }
+        else console.log("Metamask is not connected!")
     }
 
     const notLogedPanel = () => {
@@ -27,16 +52,28 @@ function Login() {
         return (
             <div className={styles.div}>
                 <div className={styles.title}>
-                    <h1 >Wallet is connected</h1>
+                    <h1>Pre Sale</h1>
                 </div>
                 <div className={styles.windowContainer}>
-                    <div style={{ margin: '2rem' }}>
-                        <PartListView tokens={myTokens} />
+                    <div className={styles.boxes}>
+                        <div className={styles.lootbox}>
+                            <span className={styles.text}>Silver Box</span>
+                            <Image src={"/icons/loot-silver.png"} width={180} height={180} alt="" />
+                            <Button modifier={styles.buyButton} onClick={handleMint} primary>Buy</Button>
+                        </div>
+                        <div className={styles.lootbox}>
+                            <span className={styles.text}>Gold Box</span>
+                            <Image className={styles.boxImage} src={"/icons/loot-gold.png"} width={180} height={180} alt="" />
+                            <Button modifier={styles.buyButton} onClick={handleMint} primary>Buy</Button>
+                        </div>
                     </div>
-                    <Mint />
+                    <span className={styles.span}>A Warbot is a set of 5 parts (Head, Body, Right Arm, Left Arm and Legs) joined in a single robot. These 5 parts are NFTs, and can be freely exchanged by the player, allowing customization of your Warbot. You can acquire a complete Warbot when purchasing a Gold Box, or a random part when purchasing a Silver Box.</span>
+                    {/* <div style={{ margin: '2rem' }}>
+                        <PartListView tokens={myTokens} />
+                    </div> */}
+                    {/* <Mint /> */}
                 </div>
             </div>
-
         )
     }
 
