709955827798447406101e5c4979f45e7e45a8ea
Lucas Lima <lucas85235@gmail.com>
2022-10-20 20:57:08 -0400

    Add head info in pre sale page

--- pages/pre-sale.jsx
+++ pages/pre-sale.jsx
@@ -1,4 +1,5 @@
 import styles from "../styles/PreSale.module.css"
+import Head from 'next/head'
 import { BlockchainProvider } from "../blockchain"
 import Login from "../components/Login"
 
@@ -6,6 +7,12 @@ function PreSale() {
     return (
 
         <div className={styles.container}>
+            <Head>
+                <title>Warbots Pre Sale</title>
+                <meta name="description" content="Warbots nft pre sale" />
+                <link rel="icon" href="/icons/logo.png" />
+            </Head>
+
             <BlockchainProvider>
                 <Login></Login>
             </BlockchainProvider>
